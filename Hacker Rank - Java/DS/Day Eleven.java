898ef4c32858cd3bb888382b1b69dcd90465fe6c
Agampreet Singh Bajwa <agambajwa2207@gmail.com>
2020-04-16 18:38:59 +0530

    New solutions

---
+++ Hacker Rank - Java/DS/Day Eleven.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[][] arr = new int[6][6];
+
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowItems[j]);
+                arr[i][j] = arrItem;
+            }
+        }
+        int sum=-1000;
+            for(int i =0 ; i<4;i++){
+                for(int x =0 ; x<4; x++){
+                    
+                    int top = arr[i][x]+arr[i][x+1]+arr[i][x+2];
+                    int middle = arr[i+1][x+1];
+                    int bottom = arr[i+2][x]+arr[i+2][x+1]+arr[i+2][x+2];
+                    if(top+middle+bottom>sum){sum=top+middle+bottom;}
+                }
+            }
+            System.out.println(sum);
+        scanner.close();
+    }
+}
---
+++ Hacker Rank - Java/DS/Day Ten.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        int count = 0;
+        int max   = 0;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                count++;
+                max = Math.max(max, count);
+            } else {
+                count = 0;
+            }
+            n = n >> 1;
+            //System.out.println(n);
+        }
+        System.out.println(max);
+        scanner.close();
+    }
+}
